971452fcf846a951eaf5b3450c37f85e1c566322
DanCeban01 <90266227+DanCeban01@users.noreply.github.com>
2021-10-12 14:27:55 +0300

    Update and rename Exercise I to ExerciseI.java

--- Laborator 1/ExerciseI.java
+++ Laborator 1/ExerciseI.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class ex1 {
-		public static void main(String[] args) {
-		    Scanner input=new Scanner(System.in);
-			Monitor mon1=new Monitor();
-			System.out.print("enter the nr of colors of display 1 (integer) : ");
-			mon1.color= input.nextInt();
-			System.out.print("enter the dimension of display 1 (float number ): ");
+public static void main(String[] args) {
+     Scanner input=new Scanner(System.in);
+	Monitor mon1=new Monitor();
+     System.out.print("enter the nr of colors of display 1 (integer) : ");
+	mon1.color= input.nextInt();
+     System.out.print("enter the dimension of display 1 (float number ): ");
 			mon1.dimension= input.nextDouble();
-			System.out.print("enter the resolution of display 1 (integer): ");
+     System.out.print("enter the resolution of display 1 (integer): ");
 			mon1.resolution= input.nextInt();
-			Monitor mon2=new Monitor();
+     Monitor mon2=new Monitor();
 			System.out.print("enter the nr of colors of display 2 (integer) : ");
 			mon2.color= input.nextInt();
 			System.out.print("enter the dimension of display 2 (float number ): ");
