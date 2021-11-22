958aef365902febb6362ffccfa6f1f24f7559953
Dan Ceban <90266227+DanCeban01@users.noreply.github.com>
2021-11-22 15:52:39 +0200

    Create Patient.java

---
+++ Laborator 6/Patient.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+public class Patient extends Person
+{
+    String id;
+    String fullname ;
+    String gender;
+    Date birthDay = new Date();;
+    int age;
+    Date acceptedDate= new Date();
+    String sickness[];
+    String prescriptions[];
+    String allergies[];
+    String specialReqs[];
+    OperStaff op[];
+}
