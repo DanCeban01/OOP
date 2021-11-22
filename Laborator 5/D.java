00c8a819d9ae403cf0c3f9e3269acdeea4851e47
DanCeban01 <90266227+DanCeban01@users.noreply.github.com>
2021-11-22 12:16:41 +0200

    Create D.java

---
+++ Laborator 5/D.java
@@ -0,0 +1,11 @@
+class D extends C
+{
+    protected String d;
+    protected X x = new X("x1x");
+    
+    D(String a, String x, String b, String c, String d) 
+    {
+        super(a, x, b, c);
+        this.d=d;
+    }
+}
