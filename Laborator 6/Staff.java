ead0cc57d838ab45796772985337eeb91cc72a2c
Dan Ceban <90266227+DanCeban01@users.noreply.github.com>
2021-11-22 15:54:57 +0200

    Create Staff.java

---
+++ Laborator 6/Staff.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+public class Staff extends Person
+{
+    Date joined = new Date();
+    
+    String education[];
+    String certification[];
+    String languages[];
+    
+    Dep d[] ;
+}
