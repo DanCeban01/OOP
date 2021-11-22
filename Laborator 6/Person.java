2a09f20d8bf6017204dd3f182d4c8b556b1ca956
Dan Ceban <90266227+DanCeban01@users.noreply.github.com>
2021-11-22 15:53:30 +0200

    Create Person.java

---
+++ Laborator 6/Person.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+public class Person 
+{
+    String title;
+    String Name;
+    String middleName;
+    String familyName;
+    String fullName;
+    
+    Date birthDate = new Date();
+    
+    String gender;
+    String address;
+    String Phone;
+    
+    Hospital h[];
+}
